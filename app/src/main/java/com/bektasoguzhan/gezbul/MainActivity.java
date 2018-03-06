5e504443c2746bc84d78bfbfcf0d545aef1bd98d
Oğuzhan BEKTAŞ <bforbektas@hotmail.com>
2018-03-06 18:21:00 +0300

    view show ve hide metodları yazıldı info değişkeni oluşturuldu giriş yapanın türünü belli etmek için. ve lauyout lara görüntü işlemleri ile ilgili ayarlar yapıldı.

--- app/src/main/java/com/bektasoguzhan/gezbul/MainActivity.java
+++ app/src/main/java/com/bektasoguzhan/gezbul/MainActivity.java
@@ -39,10 +39,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private static final int requestCode_SIGN_IN = 1461;//İstediğimiz değeri verebiliriz sabit olmak zorunda (on Aktivity Result için)
     private GoogleApiClient mGoogleApiClient;
     //Buradan sonrası gerekli tanımlamalar
-    private TextView mTextViewAd;
-    private Button mSignInButtonGoogle, mSignOutButtonGoogle, mSignInButtonFacebook, mSignOutButtonFacebook;
+    private TextView mTextViewAd, mDurumTextView, mGirisTextview;
+    private Button mSignInButtonGoogle, mSignOutButton, mSignInButtonFacebook, mDevamButton;
     private String KullaniciID = "null";
     private ImageView mImageView;
+    String info = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                     FirebaseUser user = firebaseAuth.getCurrentUser();
                     if (user != null) {
                         Log.e("Firebase Login", "Kullanıcı Artık Yetkili. Kullanıcı ID : " + user.getUid() + " Email->" + user.getEmail() + " Name->" + user.getDisplayName() + "foto URL->" + user.getPhotoUrl());
-                        mTextViewAd.setText("Hoşgeldiniz " + user.getDisplayName());
+                        mTextViewAd.setText("Sn. " + user.getDisplayName());
                         KullaniciID = user.getUid();
                         final String imgURL = String.valueOf(user.getPhotoUrl());
                         new DownLoadImageTask(mImageView).execute(imgURL);//Profil fotosu için fonksiyon
@@ -110,15 +111,40 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private void start() {//Her view in tanımı yapıldı
         mTextViewAd = (TextView) findViewById(R.id.nameTextview);
         mSignInButtonGoogle = (Button) findViewById(R.id.signIn_button);
-        mSignOutButtonGoogle = (Button) findViewById(R.id.signOut_button);
+        mSignOutButton = (Button) findViewById(R.id.signOut_button);
         mSignInButtonFacebook = (Button) findViewById(R.id.signIn_button_facebook);
-        mSignOutButtonFacebook = (Button) findViewById(R.id.signOut_button_facebook);
         mImageView = (ImageView) findViewById(R.id.imageView);
+        mDevamButton = (Button) findViewById(R.id.buttonDevam);
+        mDurumTextView = (TextView) findViewById(R.id.durumTextview);
+        mGirisTextview = (TextView) findViewById(R.id.girisTextview);
+        hide();
+    }
+
+    private void hide() {
+        //Visible işlemleri
+        mTextViewAd.setVisibility(View.INVISIBLE);
+        mSignOutButton.setVisibility(View.INVISIBLE);
+        mImageView.setVisibility(View.INVISIBLE);
+        mDurumTextView.setVisibility(View.INVISIBLE);
+        mDevamButton.setVisibility(View.INVISIBLE);
+        mSignInButtonFacebook.setVisibility(View.VISIBLE);
+        mSignInButtonGoogle.setVisibility(View.VISIBLE);
+        mGirisTextview.setVisibility(View.VISIBLE);
+    }
+
+    private void show() {
+        mTextViewAd.setVisibility(View.VISIBLE);
+        mSignOutButton.setVisibility(View.VISIBLE);
+        mImageView.setVisibility(View.VISIBLE);
+        mDurumTextView.setVisibility(View.VISIBLE);
+        mDevamButton.setVisibility(View.VISIBLE);
+        mSignInButtonFacebook.setVisibility(View.INVISIBLE);
+        mSignInButtonGoogle.setVisibility(View.INVISIBLE);
+        mGirisTextview.setVisibility(View.INVISIBLE);
     }
 
     public void gecis(View view) {//Google Map ekranına geçiş
-        //KullaniciID yolla.
-        try {
+        try {//KullaniciID yolla.
             Intent i = new Intent(MainActivity.this, SelectionActivity.class);
             i.putExtra("kullaniciID", KullaniciID);
             startActivity(i);
@@ -130,67 +156,83 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
     public void signInFacebook(View view) {//Facebook ile giriş
+        try {
+            info = "facebook";
+            show();//Şimdilik startAcrivityForResult yapıldıktan sonra silinecek
 
-    }
-
-    public void signOutFacebook(View view) {//Facebook için çıkış
-
+        } catch (Exception ex) {
+            Log.e("Facebook ile Giriş", ex.toString());
+        }
     }
 
     public void signIn(View view) {//google+ ile giriş
         try {
+            info = "google";
             Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
             startActivityForResult(signInIntent, requestCode_SIGN_IN);
         } catch (Exception ex) {
-            Log.e("Sign in de hata", ex.toString());
+            Log.e("Google+ ile Giriş", ex.toString());
         }
     }
 
     public void signOut(View view) {//google+ için çıkış
-        try {
-            mAuth.signOut();
-            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                    new ResultCallback<Status>() {
-                        @Override
-                        public void onResult(@NonNull Status status) {
-                            Log.e("Google Login", "Google oturumu kapatıldı. " + status.getStatus());
-                        }
-                    });
-            KullaniciID = "null";
-        } catch (Exception ex) {
-            Log.e("Sign out da hata", ex.toString());
+        if (info == "google") {
+            try {
+                mAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(@NonNull Status status) {
+                                Log.e("Google Login", "Google oturumu kapatıldı. " + status.getStatus());
+                            }
+                        });
+                KullaniciID = "null";
+                hide();
+            } catch (Exception ex) {
+                Log.e("Sign out da hata", ex.toString());
+            }
+        } else if (info == "facebook") {
+
+            hide();
+        } else {
+            Log.e("Out", "Doğru değer alınamıyor");
         }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {//Google için bağlantı
         super.onActivityResult(requestCode, resultCode, data);
-        try {
-            if (requestCode == requestCode_SIGN_IN) {
-                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-                if (result.isSuccess()) {
-                    Log.e("Google Login", "Oturum Açılıyor..");
-                    GoogleSignInAccount account = result.getSignInAccount();
-                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-                    mAuth.signInWithCredential(credential)
-                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        Log.e("Google Login", "Oturum Google Hesabı ile açıldı.");
-                                    } else {
-                                        Log.e("Google Login", "Oturum Açılamadı.", task.getException());
-                                        Toast.makeText(MainActivity.this, "Bağlantısı Hatası.",
-                                                Toast.LENGTH_SHORT).show();
+        if (info == "google") {
+            try {
+                if (requestCode == requestCode_SIGN_IN) {
+                    GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                    if (result.isSuccess()) {
+                        Log.e("Google Login", "Oturum Açılıyor..");
+                        GoogleSignInAccount account = result.getSignInAccount();
+                        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                        mAuth.signInWithCredential(credential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.e("Google Login", "Oturum Google Hesabı ile açıldı.");
+                                        } else {
+                                            Log.e("Google Login", "Oturum Açılamadı.", task.getException());
+                                            Toast.makeText(MainActivity.this, "Bağlantısı Hatası.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
                                     }
-                                }
-                            });
-                } else {
-                    Log.e("Google Login", "Google hesabıyla oturum açma isteği yapılamadı.");
+                                });
+                        show();
+                    } else {
+                        Log.e("Google Login", "Google hesabıyla oturum açma isteği yapılamadı.");
+                    }
                 }
+            } catch (Exception ex) {
+                Log.e("ForResult", ex.toString());
             }
-        } catch (Exception ex) {
-            Log.e("ForResult", ex.toString());
+        } else if (info == "facebook") {
+            show();
         }
     }
 
