defc6a71d5767d44ad85f30539dad8f59e4132f5
Oğuzhan BEKTAŞ <bforbektas@hotmail.com>
2018-03-30 22:49:10 +0300

    denemeler

--- app/src/main/java/com/bektasoguzhan/gezbul/MapsActivity.java
+++ app/src/main/java/com/bektasoguzhan/gezbul/MapsActivity.java
@@ -2,9 +2,13 @@ package com.bektasoguzhan.gezbul;
 
 import android.*;
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,12 +25,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -36,14 +49,23 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapLongClickListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         com.google.android.gms.location.LocationListener,
         GoogleMap.OnMarkerClickListener,
         GoogleMap.OnMarkerDragListener {
     private static String LOG_TAG = "MapsActivity";
+    int PLACE_PICKER_REQUEST = 1;
     private GoogleMap mMap;
     LocationManager mLocationManager;
     LocationListener mLocationListener;
@@ -56,6 +78,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     int PROXIMITY_RADIUS = 10000;//Sen çapın
     double latitude, longitude, end_latitude, end_longitude;//Konum değişkenleri
 
+    // The entry points to the Places API.
+    private GeoDataClient mGeoDataClient;
+    private PlaceDetectionClient mPlaceDetectionClient;
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    //Döndürülecek sonuç adeti
+    private static final int M_MAX_ENTRIES = 10;
+    //Gerekli Dizilerin Tanımlanması
+    private String[] mLikelyPlaceNames;
+    private String[] mLikelyPlaceAddresses;
+    private String[] mLikelyPlaceAttributions;
+    private LatLng[] mLikelyPlaceLatLngs;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -67,6 +102,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         start();
         autoComplete();
+        //GeoDataClient, Google'ın yerel yer ve iş bilgileri veritabanına erişim sağlar.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        //cihazın geçerli yerine hızlı erişim sağlar ve cihazın yerini belirli bir yerde bildirme fırsatı sunar.
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // Construct a FusedLocationProviderClient.
+        //Android konum hizmetleri için ana giriş noktasıdır.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
     private void start() {//Tanımlamalar
@@ -79,6 +125,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void fill_select(String select) {
+        mMap.clear();
         if (select == "none") {
 
         } else {
@@ -86,13 +133,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
             Toast.makeText(MapsActivity.this, select, Toast.LENGTH_SHORT).show();
             String url = getUrl(latitude, longitude, select);
-            mMap.clear();
+
             dataTransfer[0] = mMap;
             dataTransfer[1] = url;
 
             getNearbyPlacesData.execute(dataTransfer);
         }
-        select = "none";
     }
 
     private void autoComplete() {//Otomatik arama yapmak için
@@ -120,7 +166,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setOnMapLongClickListener(this);//Haritaya uzun basıldığında işlem yapabilmek için
         //Kullanıcı iznini alma
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -142,6 +188,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMyLocationEnabled(true);
         mMap.setOnMarkerDragListener(this);
         mMap.setOnMarkerClickListener(this);
+        // Do other setup activities here too, as described elsewhere in this tutorial.
+     /*   mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {//Dialog açılması için
+
+            @Override
+            // Return null here, so that getInfoContents() is called next.
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Inflate the layouts for the info window, title and snippet.
+                View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+
+                TextView title = ((TextView) infoWindow.findViewById(R.id.title));
+                title.setText(marker.getTitle());
+
+                TextView snippet = ((TextView) infoWindow.findViewById(R.id.snippet));
+                snippet.setText(marker.getSnippet());
+
+                return infoWindow;
+            }
+        });*/
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -177,6 +246,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions()
+                        .position(place.getLatLng())
+                        .title(place.getName().toString())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                //  String toastMsg = String.format("Place: %s", place.getName());
+                // Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
@@ -262,6 +346,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private String getUrl(double latitude, double longitude, String nearbyPlace) {//Yakındaki yerleri çektiğimiz yer.
+        Log.i("gelen tür", nearbyPlace);
         StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
         googlePlacesUrl.append("location=" + latitude + "," + longitude);
         googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
@@ -271,4 +356,184 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Log.d("getUrl", googlePlacesUrl.toString());
         return (googlePlacesUrl.toString());
     }
+
+    private void showCurrentPlace(LatLng latLng) {
+        if (mMap == null) {
+            return;
+        }
+        @SuppressWarnings("MissingPermission") final Task<PlaceLikelihoodBufferResponse> placeResult =
+                mPlaceDetectionClient.getCurrentPlace(null);
+        placeResult.addOnCompleteListener
+                (new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                    @Override
+                    public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+
+                            // Set the count, handling cases where less than 5 entries are returned.
+                            int count;
+                            if (likelyPlaces.getCount() < M_MAX_ENTRIES) {
+                                count = likelyPlaces.getCount();
+                            } else {
+                                count = M_MAX_ENTRIES;
+                            }
+
+                            int i = 0;
+                            mLikelyPlaceNames = new String[count];
+                            mLikelyPlaceAddresses = new String[count];
+                            mLikelyPlaceAttributions = new String[count];
+                            mLikelyPlaceLatLngs = new LatLng[count];
+
+                            for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                                // Build a list of likely places to show the user.
+                                mLikelyPlaceNames[i] = (String) placeLikelihood.getPlace().getName();
+                                mLikelyPlaceAddresses[i] = (String) placeLikelihood.getPlace()
+                                        .getAddress();
+                                mLikelyPlaceAttributions[i] = (String) placeLikelihood.getPlace()
+                                        .getAttributions();
+                                mLikelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
+                                Log.e("deneme", mLikelyPlaceNames[i] + " : " + mLikelyPlaceAddresses[i]);
+                                i++;
+                                if (i > (count - 1)) {
+                                    break;
+                                }
+                            }
+
+                            // Release the place likelihood buffer, to avoid memory leaks.
+                            likelyPlaces.release();
+
+                            // Show a dialog offering the user the list of likely places, and add a
+                            // marker at the selected place.
+                            openPlacesDialog();
+
+                        } else {
+                            Log.e("showCurrentPlace", "Exception: %s", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void openPlacesDialog() {
+        // Ask the user to choose the place where they are now.
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // The "which" argument contains the position of the selected item.
+                LatLng markerLatLng = mLikelyPlaceLatLngs[which];
+                String markerSnippet = mLikelyPlaceAddresses[which];
+                if (mLikelyPlaceAttributions[which] != null) {
+                    markerSnippet = markerSnippet + "\n" + mLikelyPlaceAttributions[which];
+                }
+
+                // Add a marker for the selected place, with an info window
+                // showing information about that place.
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions()
+                        .title(mLikelyPlaceNames[which])
+                        .position(markerLatLng)
+                        .snippet(markerSnippet));
+
+                // Position the map's camera at the location of the marker.
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng,
+                        17));
+            }
+        };
+
+        // Display the dialog.
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Lütfen Gitmek istediğiniz yeri seçiniz.")
+                .setItems(mLikelyPlaceNames, listener)
+                .show();
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        //  showCurrentPlace(latLng);
+        //Adres Bulma işlemleri
+    /*    mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(latLng.toString())
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));*/
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("onMapLongClick", "Haritaya uzun basıldı");
+
+     /*   Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address = "";
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if (addressList != null && addressList.size() > 0) {
+                if (addressList.get(0).getThoroughfare() != null) {
+                    address += addressList.get(0).getThoroughfare();
+
+                    if (addressList.get(0).getSubThoroughfare() != null) {
+                        address += addressList.get(0).getSubThoroughfare();
+                    }
+                }
+            } else {
+                address = "Bilinmiyor";
+            }
+            Log.d("addres",address);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+        //DENEME
+      /*  Geocoder gc = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = gc.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            StringBuilder sb = new StringBuilder();
+            if (addresses.size() > 0) {
+                Address address = addresses.get(0);
+                for (int i = 0; i < address.getMaxAddressLineIndex(); i++)
+                    sb.append(address.getAddressLine(i)).append("\n");
+                sb.append(address.getAddressLine(0)).append("\n");
+
+            }
+            Log.d("addres", sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+*/
+        //---
+        // mMap.clear();
+        //   mMap.addMarker(new MarkerOptions().position(latLng).title("in"));
+
+        ///
+
+      /*  AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+        builder.setTitle("GEZBUL");
+        builder.setMessage("Bu yere puan vermek ya da düzenlemek ister misiniz ? ");
+        builder.setNegativeButton("İPTAL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //İptal butonuna basılınca yapılacaklar.Sadece kapanması isteniyorsa boş bırakılacak
+                Toast.makeText(MapsActivity.this, "İptal Edildi", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        builder.setPositiveButton("EVET", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //Tamam butonuna basılınca yapılacaklar
+                Toast.makeText(MapsActivity.this, "EVET", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();*/
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+
 }
